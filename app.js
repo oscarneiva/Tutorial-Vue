f807eedf3f3f8a0f8890cc6384e04a8f06890205
oscar-neiva <oscarneivaeneto@gmail.com>
2017-07-20 16:19:04 -0300

    lesson 14

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-new Vue({
-    el: '#vue-app',
-    data:{
-        health: 100,
-        ended = false,
+var one = new Vue({
+    el: '#vue-app-one',
+    data: {
+        title: 'Vue App One'
     },
-    methods:{
-        punch: function(){
-            this.health -= 10; 
-            if (this.health <= 0){
-                this.ended = true;
-            }
-        },
-        restart: function(){
-            this.health = 100;
-            this.ended = false;
-        }
-    },
-    computed:{
+    methods: {
         
+    },
+    computed: {
+        greet: function(){
+            return 'Hello from app one :)';
+        }
     }
 });
+
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {
+        title: 'Vue App Two'
+    },
+    methods: {
+        changeTitle: function(){
+            one.title = "Title changed";
+        }
+    },
+    computed: {
+        greet: function(){
+            return 'Hello from add two :)';
+        }
+    }
+});
+
+two.title = "Change from outside";
