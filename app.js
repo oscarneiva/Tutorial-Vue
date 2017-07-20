39bab1aaf7345c4fad80dc64e15661f255d78409
oscar-neiva <oscarneivaeneto@gmail.com>
2017-07-20 16:40:21 -0300

    lesson 15

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
-var one = new Vue({
+
+Vue.component('greeting', {
+    template: '<p>Hey there, I am {{name}}.<button v-on:click="changeName">Change Name</button></p>',
+    data: function(){
+        return{
+            name: 'Yoshi'
+        }
+    },
+    methods:{
+        changeName: function(){
+            this.name = 'Mario';
+        }
+    }
+});
+
+new Vue({
     el: '#vue-app-one',
-    data: {
-        title: 'Vue App One'
-    },
-    methods: {
-        
-    },
-    computed: {
-        greet: function(){
-            return 'Hello from app one :)';
-        }
-    }
 });
 
-var two = new Vue({
+new Vue({
     el: '#vue-app-two',
-    data: {
-        title: 'Vue App Two'
-    },
-    methods: {
-        changeTitle: function(){
-            one.title = "Title changed";
-        }
-    },
-    computed: {
-        greet: function(){
-            return 'Hello from add two :)';
-        }
-    }
 });
-
-two.title = "Change from outside";
