d11018e0551f85aa7ef8be8c49c51bf6aedd79be
oscar-neiva <oscarneivaeneto@gmail.com>
2017-07-20 17:16:33 -0300

    lesson 16

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-
-Vue.component('greeting', {
-    template: '<p>Hey there, I am {{name}}.<button v-on:click="changeName">Change Name</button></p>',
-    data: function(){
-        return{
-            name: 'Yoshi'
-        }
+new Vue({
+    el: '#vue-app',
+    data:{
+        output: 'your fav food'
     },
     methods:{
-        changeName: function(){
-            this.name = 'Mario';
+        readRefs:function(){
+            console.log(this.$refs.test.innerText);
+            this.output = this.$refs.input.value
         }
     }
-});
-
-new Vue({
-    el: '#vue-app-one',
-});
-
-new Vue({
-    el: '#vue-app-two',
 });
