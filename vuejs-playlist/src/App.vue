ca0055e3f79467d5e4a85282332c2fcf41b15edf
oscar-neiva <oscarneivaeneto@gmail.com>
2017-07-21 20:45:53 -0300

    lesson 22

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,37 +1,40 @@
 <template>
-  <div>
-    <app-header></app-header>
-    <app-ninjas></app-ninjas>
-    <app-footer></app-footer>
-  </div>
+    <div>
+        <app-header></app-header>
+        <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
+// Imports
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
-import Ninjas from './components/Ninjas.vue'
-
-export default{
-  components:{
+import Ninjas from './components/Ninjas.vue';
+export default {
+    components: {
         'app-header': Header,
         'app-footer': Footer,
         'app-ninjas': Ninjas
-  },
-  data(){
-    return{
-      title: 'Vue Ninjas'
+    },
+    data () {
+        return {
+          ninjas: [
+              {name: 'Ryu', speciality: 'Vue Components', show: false},
+              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+              {name: 'Hitoshi', speciality: 'Click Events', show: false},
+              {name: 'Tango', speciality: 'Conditionals', show: false},
+              {name: 'Kami', speciality: 'Webpack', show: false},
+              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+          ]
+        }
     }
-  }
 }
 </script>
 
 <style>
-header{
-  background: lightgreen;
-  padding: 10px;
-}
-h1{
-  color: #222;
-  text-align: center;
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
 }
 </style>
--- vuejs-playlist/src/components/Ninjas.vue
+++ vuejs-playlist/src/components/Ninjas.vue
@@ -10,18 +10,16 @@
 </template>
 <script>
 export default {
-    data() {
-      return{
-          ninjas:{
-              {name: 'Ryu', speciality: 'Vue components', show: false},
-              {name: 'Crystal', speciality: 'HTML Wizard', show: false},
-              {name: 'Hitoshi', speciality: 'Click Events', show: false},
-              {name: 'Tango', speciality: 'Conditionals', show: false},
-              {name: 'Kami', speciality: 'Webpack', show: false},
-              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-          }
+    props: {
+      ninjas: {
+        type: Array,
+        required: true
       }
-    } 
+    },
+    data(){
+        return{
+        }
+    }
 }
 </script>
 <style scoped>
